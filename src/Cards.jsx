6775a4df2ac3487d1e60d0a7c65bfa37f4bfbb58
ad551 <gdaim551@gmail.com>
2022-02-24 03:29:08 +0500

    earliest commit

---
+++ src/Cards.jsx
@@ -0,0 +1,20 @@
+function Card(props){
+    return(
+      <>
+      <div className='cards'>
+        <div className='card'>
+          <img src={props.imgsrc} alt="Image" className='card__img'/>
+          <div className='card__info'>
+            <span className='card__category'>{props.title}</span>
+            <h3 className='card__title'>{props.plname}</h3>
+            <a href={props.link} target='_blank'>
+              <button>{props.webname}</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+  }
+
+  export default Card;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from "./Cards";
+import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>Top 3 Programming Languages</h1>
+    <Card imgsrc="https://wallpapercave.com/dwp1x/wp2465923.jpg"
+    title="Programming Series"
+    plname="ReactJS"
+    link="https://reactjs.org/"
+    webname="React Website"
+    />
+
+    <Card imgsrc="https://4beginner.blob.core.windows.net/article/JavaScript-Logo_051220_105700023.png"
+    title="Programming Series"
+    plname="JavaScript"
+    link="https://www.javascript.com/"
+    webname="JavaScript Website"
+    />
+
+    <Card imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/300px-Python-logo-notext.svg.png"
+    title="Programming Series"
+    plname="Python"
+    link="https://www.python.org/"
+    webname="Python Website"
+    />
+  </>
+  ,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
