0d6cde3e9f4986d242373a8c017a8825ce9869a9
ad551 <gdaim551@gmail.com>
2022-02-26 00:42:30 +0500

    map and fat arrow function

---
+++ src/Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata=[
+    {
+        plname: "ReactJS",
+        imgsrc: "https://wallpapercave.com/dwp1x/wp2465923.jpg",
+        title: "Programming Language",
+        link: "https://reactjs.org/",
+        webname: "React Website"
+    },
+    {
+        plname: "JavaScript",
+        imgsrc: "https://4beginner.blob.core.windows.net/article/JavaScript-Logo_051220_105700023.png",
+        title: "Programming Language",
+        link: "https://www.javascript.com/",
+        webname: "JavaScript Website"
+    },
+    {
+        plname: "Python",
+        imgsrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/300px-Python-logo-notext.svg.png",
+        title: "Programming Language",
+        link: "https://www.python.org/",
+        webname: "Python Website"
+    }
+];
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from "./Cards";
-import './index.css'
+import './index.css';
+import Sdata from './Sdata';
+
+// function ncard(val){
+//   return(
+//     <Card 
+//     imgsrc={val.imgsrc}
+//     title={val.title}
+//     plname={val.plname}
+//     link={val.link}
+//     webname={val.webname}
+//     />    
+//   );
+// }
 
 ReactDOM.render(
   <>
-    <h1>Top 3 Programming Languages</h1>
-    <Card imgsrc="https://wallpapercave.com/dwp1x/wp2465923.jpg"
-    title="Programming Series"
-    plname="ReactJS"
-    link="https://reactjs.org/"
-    webname="React Website"
-    />
-
-    <Card imgsrc="https://4beginner.blob.core.windows.net/article/JavaScript-Logo_051220_105700023.png"
-    title="Programming Series"
-    plname="JavaScript"
-    link="https://www.javascript.com/"
-    webname="JavaScript Website"
-    />
-
-    <Card imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/300px-Python-logo-notext.svg.png"
-    title="Programming Series"
-    plname="Python"
-    link="https://www.python.org/"
-    webname="Python Website"
-    />
+    <h1 className='main_heading'>Top 3 Programming Languages</h1>
+    {Sdata.map((val, index) =>{
+      return(
+        <Card 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        plname={val.plname}
+        link={val.link}
+        webname={val.webname}
+        />   
+      ); 
+    })}
   </>
   ,
   document.getElementById('root')
