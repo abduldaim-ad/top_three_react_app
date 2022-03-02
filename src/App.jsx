6ca851dcaf073c0119a788df2c38305923638b52
ad551 <gdaim551@gmail.com>
2022-03-02 15:04:21 +0500

    else-if and ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,25 @@ import Sdata from './Sdata';
 //   );
 // }
 
+const favPL = 'ReactJS';
+const FavoritePL = () =>{
+if(favPL==='ReactJS'){
+    return (<h1>My favorite programming language is {favPL}</h1>);
+}else if(favPL==='JavaScript'){
+    return (<h1>My favorite programming language is {favPL}</h1>);
+}else if(favPL==='Python'){
+    return (<h1>My favorite programming language is {favPL}</h1>);
+}
+else{
+    return (<h1>Invalid</h1>);
+}
+}
+
 const App = ()=>(
   <>
     <h1 className='main_heading'>Top 3 Programming Languages</h1>
+    <FavoritePL/>
+    {/* {(favPL==='ReactJS') ? (<h1>My favorite programming language is {favPL}</h1>) : (<h1>Invalid</h1>)} */}
     {Sdata.map((val, index) =>{
       return(
         <Card 
